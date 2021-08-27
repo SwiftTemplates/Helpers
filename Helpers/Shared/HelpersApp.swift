f2552f07c031bae7fc922d56a5c39db8c315907e
Justin Bush <revblaze@users.noreply.github.com>
2021-08-27 14:32:01 -0400

    Create Universal project

---
+++ Helpers/Shared/HelpersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelpersApp.swift
+//  Shared
+//
+//  Created by Justin Bush on 2021-08-27.
+//
+
+import SwiftUI
+
+@main
+struct HelpersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
